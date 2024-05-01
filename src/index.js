502bb7e35145b80d55919cfa8b7954dd5910dd2d
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-01 14:39:39 +0100

    routes added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
 import reportWebVitals from "./reportWebVitals";
 import Root from "./components/Root/Root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
 
+const router = createBrowserRouter(routes);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
