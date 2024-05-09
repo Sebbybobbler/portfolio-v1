e597f1fd7ce34a509fd214919dd06bd1adee2913
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-09 17:06:39 +0100

    continued edit on home page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,45 +9,51 @@ function ProjectCard() {
     {
       title: "Peeling Good",
       image: banana,
-      description: "A group frontend project designed as part of the TechNative Academy. ",
-      alt: "homepage of banana merchants website"
+      description:
+        "A group frontend project designed as part of the TechNative Academy. ",
+      alt: "homepage of banana merchants website",
+      link: "https://github.com/Sebbybobbler/portfolio-banana",
     },
     {
       title: "Solar System",
       image: planet,
-      description: "JavaScript project involving JavaScript to display different elements using the DOM.",
-      alt:"Homepage of solar system website",
+      description:
+        "JavaScript project involving JavaScript to display different elements using the DOM.",
+      alt: "Homepage of solar system website",
+      link: "https://github.com/Sebbybobbler/portfolio-solar-system"
     },
     {
-      title:"Space Adventure",
+      title: "Space Adventure",
       image: adventures,
-      description:"Frontend project utilising flexbox and responsive design.",
-      alt:"Homepage of a interspace travel agency",
+      description: "Frontend project utilising flexbox and responsive design.",
+      alt: "Homepage of a interspace travel agency",
+      link: "https://github.com/Sebbybobbler/portfolio-space-adventures"
     },
     {
-      title:"Planet Filter",
-      image:filter,
-      description:"JavaScript project experimenting with filtering elements",
-      alt:"Homepage of planets and searchbar"
-    }
+      title: "Planet Filter",
+      image: filter,
+      description: "JavaScript project experimenting with filtering elements",
+      alt: "Homepage of planets and searchbar",
+      link: "https://github.com/Sebbybobbler/portfolio-filter"
+    },
   ];
 
   const projectCard = project.map((item, index) => {
-    return(
-     <div className={styles.projectCardWrapper}>
-       <h4>{item.title}</h4>
-       <img src={item.image} alt={item.alt} />
-       <span className={styles.description}>{item.description}</span>
-     </div>
-    )
+    return (
+      <div onClick={()=>{window.open(item.link,"blank")}} className={styles.projectCardWrapper}>
+        <h4>{item.title}</h4>
+        <img src={item.image} alt={item.alt} />
+        <span className={styles.description}>{item.description}</span>
+      </div>
+    );
   });
-  return <div className={styles.projects}>{projectCard}</div>;
+  return <div  className={styles.projects}>{projectCard}</div>;
 }
 
 function Home() {
   return (
     <div className={styles.wrapper}>
-        <ProjectCard />
+      <ProjectCard />
     </div>
   );
 }
