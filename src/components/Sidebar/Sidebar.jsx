5e6e86145a03d12a576e2435d01556cdbe87c1ef
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-09 15:31:02 +0100

    background color

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,13 @@
 import styles from "./Sidebar.module.css";
+import SiteNav from "../SiteNav/SiteNav";
 
-export default function Sidebar(){
-    return(
-        <div className={styles.sidebar}><p>I'm a sidebar ...............................................................................................................................</p></div>
-    )
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.initials}>
+        <h1>SD</h1>
+      </div>
+      <SiteNav />
+    </div>
+  );
 }
